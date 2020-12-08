23e66aa1603b85d3d018b2172762bd3d130e4ede
TonyCastillo <tony.castillo.7@gmail.com>
2020-12-08 19:01:57 -0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/* SidebarNAV-Initializattion */
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+
+
+  
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  })
+
+
+        
